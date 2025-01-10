02dbb815ab64c156d81fb415066b74e8289cfa35
xGGOD_ <rblo2002@gmail.com>
2025-01-10 14:43:37 +0100

    part 6 b finished

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice, current } from '@reduxjs/toolkit'
+
 const initialState = [
   {
     content: 'reducer defines how redux store works',
@@ -34,7 +36,47 @@ const noteReducer = (state = initialState, action) => {
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
-export const createNote = (content) => {
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+
+    createNote(state, action) {
+
+      const content = action.payload
+
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      })
+
+    },
+
+
+    toggleImportanceOf(state, action) {
+
+      const id = action.payload
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+
+      console.log(state)
+
+      return state.map(note =>
+        note.id !== id ? note : changedNote
+      )
+    }
+
+  },
+})
+
+export const { createNote, toggleImportanceOf } = noteSlice.actions
+export default noteSlice.reducer
+
+/* export const createNote = (content) => {
   return {
     type: 'NEW_NOTE',
     payload: {
@@ -52,4 +94,4 @@ export const toggleImportanceOf = (id) => {
   }
 }
 
-export default noteReducer
+export default noteReducer */
